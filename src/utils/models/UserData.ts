7fc6aed06576f129e04337c79e486be054b8da4c
Julius Schwaner <jwschwaner@gmail.com>
2025-02-20 14:12:58 +0100

    added userdata model

---
+++ src/utils/models/UserData.ts
@@ -0,0 +1,43 @@
+export enum Gender {
+    male,
+    female
+}
+
+export enum ActivityLevel {
+    sedentary,
+    light,
+    moderate,
+    active,
+    veryActive
+}
+
+export enum Goal {
+    loseWeight,
+    maintainWeight,
+    gainWeight
+}
+
+export class UserDataModel {
+    gender: Gender;
+    age: number;
+    height: number; // in centimeters
+    weight: number; // in kilograms
+    activityLevel: ActivityLevel;
+    goal: Goal;
+
+    constructor(
+        gender: Gender,
+        age: number,
+        height: number,
+        weight: number,
+        activityLevel: ActivityLevel,
+        goal: Goal
+    ) {
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.activityLevel = activityLevel;
+        this.goal = goal;
+    }
+}
